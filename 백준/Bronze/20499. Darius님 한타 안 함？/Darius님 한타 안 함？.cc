7c522cd6da0d0492058f4b9bb2bea3bdadd75c61
박태수 <163130634+parktaesu123@users.noreply.github.com>
2024-05-23 16:55:19 +0900

    [Bronze IV] Title: Darius님 한타 안 함?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int K,D,A;
+	
+	scanf("%d/%d/%d",&K,&D,&A);
+	
+	if((K+A < D) || (D == 0))
+	{
+		printf("hasu");
+	}
+	else
+	{
+		printf("gosu");
+	}
+	return 0;
+}
