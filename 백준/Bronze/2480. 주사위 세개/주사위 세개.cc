7aeeefa3937f438c4ee56d486b16c3a6423048fe
박태수 <163130634+parktaesu123@users.noreply.github.com>
2024-05-26 21:17:03 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    
+    scanf("%d %d %d", &a, &b, &c);
+    
+    if (a == b && b == c) {
+        printf("%d", 10000 + a * 1000);
+    } else if (a == b || a == c) {
+        printf("%d", 1000 + a * 100);
+    } else if (b == c) {
+        printf("%d", 1000 + b * 100);
+    } else {
+        int max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+        printf("%d", max * 100);
+    }
+    
+    return 0;
+}
