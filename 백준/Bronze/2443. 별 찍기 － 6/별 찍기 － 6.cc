c2cafe2eb46484675c19b403fa5cd7f330d0608d
박태수 <163130634+parktaesu123@users.noreply.github.com>
2024-05-26 21:02:26 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	scanf("%d",&n);
+		for (int i = 0; i<n; i++)
+	{
+			for (int k = 0; k<i; k++)
+			{
+				printf(" ");
+			}
+			for(int j = n; j>i; j--)
+			{
+				printf("*");
+			}
+			for(int j = n-1; j>i; j--)
+			{
+				printf("*");
+			}
+		printf("\n");
+	}
+	return 0;
+}
