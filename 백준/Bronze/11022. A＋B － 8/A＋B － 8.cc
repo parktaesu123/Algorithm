e86ba279871c23abaeb2380533ea2143625155ca
박태수 <163130634+parktaesu123@users.noreply.github.com>
2024-05-26 23:02:24 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int n,a,b;
+	
+	scanf("%d",&n);
+	
+	for(int i = 1; i<=n; i++)
+	{
+		scanf("%d %d",&a,&b);
+		printf("Case #%d: %d + %d = %d\n",i,a,b,a+b);
+	}
+	
+	return 0;
+}
