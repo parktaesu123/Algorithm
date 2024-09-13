f1f4f583c731c1b35dd960da4082aa032e93ea06
박태수 <163130634+parktaesu123@users.noreply.github.com>
2024-09-13 09:00:14 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,9 +1,13 @@
 function solution(my_string, is_prefix) {
-    if(my_string.startsWith(is_prefix)) {
-        return 1;
+    
+    if(my_string.length < is_prefix.length){
+        return 0;
     }
     
-    else {
-      return 0;  
-    } 
+    for(let i = 0; i<is_prefix.length; i++) {
+        if(my_string[i] !== is_prefix[i])
+            return 0;
+    }
+    
+    return 1;
 }
