5f7032de7d06120fb359e397638f27501c0c38c0
박태수 <163130634+parktaesu123@users.noreply.github.com>
2024-09-26 08:39:42 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+    var answer = [];
+    num_list.sort(function (a ,b) {
+        return a - b;
+    })
+    
+    for(let i = 0; i<num_list.length; i++) {
+        if(i < 5) {
+            answer.push(num_list[i]);
+        }
+    }
+    return answer;
+}
