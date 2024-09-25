df87295f165f429f6d1a3f9898bb9715e236879e
박태수 <163130634+parktaesu123@users.noreply.github.com>
2024-09-26 08:37:23 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,16 @@
+function solution(a, b) {
+    var answer = 0;
+    if(a % 2 !== 0 && b % 2 !== 0) {
+        answer = a*a + b*b;
+    }
+    else if(a % 2 === 0 && b % 2 === 0) {
+        if(a > b)
+            answer = a - b;
+        else
+            answer = b - a;
+    }
+    else {
+        answer = 2*(a+b);
+    }
+    return answer;
+}
