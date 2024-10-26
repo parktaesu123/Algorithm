2ad1e39f76ac3105231f8ed5668fc3a197e85d2c
박태수 <163130634+parktaesu123@users.noreply.github.com>
2024-10-26 14:43:43 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -1,8 +1,4 @@
 function solution(todo_list, finished) {
     var answer = [];
-    for(let i = 0; i<todo_list.length; i++) {
-        if(!finished[i])
-            answer.push(todo_list[i]);
-    }
     return answer;
 }
