afd66483868aef5bbacecbfccf73a21e4bb85500
박태수 <163130634+parktaesu123@users.noreply.github.com>
2025-08-06 13:19:56 +0900

    [level 0] Title: 배열 만들기 3, Time: 6.49 ms, Memory: 42.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -0,0 +1,12 @@
+function solution(arr, intervals) {
+    var answer = [];
+    
+    for(let i = 0; i<intervals.length; i++) {
+        for(let j = intervals[i][0]; j<=intervals[i][1]; j++) {
+            answer.push(arr[j])
+        }
+    }
+    
+    
+    return answer;
+}
