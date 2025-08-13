0bd0a1f68e243d354a70c1fe826411c0501a8249
박태수 <163130634+parktaesu123@users.noreply.github.com>
2025-08-13 17:35:34 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 27.96 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -0,0 +1,10 @@
+function solution(arr, queries) {
+    var answer = [];
+    
+    for(let i = 0; i<queries.length; i++) {
+       for(let j = queries[i][0]; j<=queries[i][1]; j++) {
+           arr[j] += 1;
+       }
+    }
+    return arr;
+}
